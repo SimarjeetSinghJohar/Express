48acddf567cafc1afac107d57903098b9c526f30
SimarjeeSinghJohar <85599473+SimarjeeSinghJohar@users.noreply.github.com>
2022-02-23 13:30:47 -0500

    INitial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express')
+var app = express()
+app.use(express.static(__dirname))
+var server = app.listen(3000 ,() => {
+    console.log('The server is running on port',server.address().port)
+})
