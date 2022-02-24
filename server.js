90f3650950b95889f5e7436efb7e2191f0f86460
SimarjeeSinghJohar <85599473+SimarjeeSinghJohar@users.noreply.github.com>
2022-02-23 21:34:51 -0500

    parsing body

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 app.use(express.static(__dirname))
+app.use(bodyParser.json())  //app knows we are parsing the body
+app.use(bodyParser.json())  //app knows we are parsing the body
 
-app.get('/message', (req, rep) => {
-    rep.send('hello')
+var messages = [
+    {name:'Tim', message: 'Hey'},
+    {name:'Simar', message: 'Hello!!'}
+]
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+app.post('/messages' , (req, res) =>{
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 var server = app.listen(3000 ,() => {
     console.log('The server is running on port',server.address().port)
