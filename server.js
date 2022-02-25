f9a16e4530fa4b08f06a7120e2f2c1c39844146d
SimarjeeSinghJohar <85599473+SimarjeeSinghJohar@users.noreply.github.com>
2022-02-25 15:22:01 -0500

    add Promise to reduce callbacks and increase performance.

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var mongoose = require('mongoose')
+const { response } = require('express')
 app.use(express.static(__dirname))
 app.use(bodyParser.json())  //app knows we are parsing the body
 app.use(bodyParser.urlencoded({extended:false}))  
-
+ var promise = Promise
 var dbUrl = 'mongodb+srv://Simarjeet:Simarjeet@learningnode.6o8wr.mongodb.net/LearningNode'
 var Message = mongoose.model("Message", {
     name: String,
@@ -24,15 +25,21 @@ app.get('/messages', (req, res) => {
 })
 app.post('/messages' , (req, res) =>{
     var message = new Message(req.body)
+    message .save()
+    .then(() => {
+    console.log('saved')
+    return Message.findOne({message: 'badword'})
 
-    message .save((err) => {
-        if(err)
-            sendStatus(500 )
-            Message.findOne('badword', (err,censored) => {
-                8
-            })
+    }).then(censored => {
+        if(censored) {
+            console.log('Censored words found', censored)
+            return Message.remove({_id: censored.id})
+        }
         io.emit('message', req.body)
         res.sendStatus(200)
+    }).catch((err)=>{
+        res.sendStatus(500)
+        return console.error(err)
     })
 })
 io.on('connection', (socket) =>{
@@ -44,3 +51,4 @@ io.on('connection', (socket) =>{
 var server = http.listen(3000, () => {
     console.log('The server is running on port',server.address().port)
 })
+
